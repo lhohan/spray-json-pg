e2cda87713d6f835d756acafe9fc8f247cb23b4b
lhohan <github@lhoest.eu>
2014-09-14 21:05:10 +0200

    Implementation example with warning.

--- src/main/scala/M2eeJsonProtocol.scala
+++ src/main/scala/M2eeJsonProtocol.scala
@@ -7,7 +7,7 @@ object M2eeJsonProtocol extends DefaultJsonProtocol {
       JsObject(m.mapValues {                  // 2
         case v: String => JsString(v)         // 3
         case v: Int => JsNumber(v)
-        case v: Map[_, _] => write(v.asInstanceOf[Map[String,Any]])  // 4
+        case v: Map[String, Any] => write(v)  // 4
         case v: Any => JsString(v.toString)   // 5
       })
     }
