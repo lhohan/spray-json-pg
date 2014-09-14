595a1aecccd69a42413123a0c88f1343da031a21
lhohan <github@lhoest.eu>
2014-09-14 21:09:14 +0200

    Implementation example without warning.

--- src/main/scala/M2eeJsonProtocol.scala
+++ src/main/scala/M2eeJsonProtocol.scala
@@ -2,12 +2,14 @@ import spray.json._
 
 object M2eeJsonProtocol extends DefaultJsonProtocol {
 
+  type StringToAny = Map[String, Any]
+
   implicit object MapJsonFormat extends JsonFormat[Map[String, Any]] { // 1
     def write(m: Map[String, Any]) = {
       JsObject(m.mapValues {                  // 2
         case v: String => JsString(v)         // 3
         case v: Int => JsNumber(v)
-        case v: Map[String, Any] => write(v)  // 4
+        case v: StringToAny => write(v)  // 4
         case v: Any => JsString(v.toString)   // 5
       })
     }
