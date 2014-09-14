798b94b60a35941c348e21d3f2e90a96ab5bf917
lhohan <github@lhoest.eu>
2014-09-14 21:45:49 +0200

    quick non-elegant fix

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ resolvers ++= Seq(
   "spray repo" at "http://repo.spray.io/"
 )
 
+scalaVersion := "2.11.2"
+
 libraryDependencies += "io.spray" %% "spray-json" % "1.2.6"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/main/scala/M2eeJsonProtocol.scala
+++ src/main/scala/M2eeJsonProtocol.scala
@@ -2,14 +2,12 @@ import spray.json._
 
 object M2eeJsonProtocol extends DefaultJsonProtocol {
 
-  type StringToAny = Map[String, Any]
-
   implicit object MapJsonFormat extends JsonFormat[Map[String, Any]] { // 1
     def write(m: Map[String, Any]) = {
       JsObject(m.mapValues {                  // 2
         case v: String => JsString(v)         // 3
         case v: Int => JsNumber(v)
-        case v: StringToAny => write(v)  // 4
+        case v: Map[_, _] => write(v.asInstanceOf[Map[String, Any]])  // 4
         case v: Any => JsString(v.toString)   // 5
       })
     }
