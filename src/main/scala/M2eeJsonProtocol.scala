1bdea9a01454c13da01621adfdea6663b0c181f7
lhohan <github@lhoest.eu>
2014-09-14 16:37:44 +0200

    Refactoring: mapValues and ugly removal of compiler warning.

--- src/main/scala/M2eeJsonProtocol.scala
+++ src/main/scala/M2eeJsonProtocol.scala
@@ -4,16 +4,14 @@ object M2eeJsonProtocol extends DefaultJsonProtocol {
 
   implicit object MapJsonFormat extends JsonFormat[Map[String, Any]] { // 1
     def write(m: Map[String, Any]) = {
-      JsObject(m.map {                                                 // 2
-        case (k, v) => v match {
-          case v: String => (k, JsString(v))                           // 3
-          case v: Int => (k, JsNumber(v))
-          case v: Map[String, Any] => (k, write(v))                    // 4
-          case _ => (k, JsString(v.toString))                          // 5
-        }
+      JsObject(m.mapValues {                  // 2
+        case v: String => JsString(v)         // 3
+        case v: Int => JsNumber(v)
+        case v: Map[_, _] => write(v.asInstanceOf[Map[String,Any]])  // 4
+        case v: Any => JsString(v.toString)   // 5
       })
     }
 
-    def read(value: JsValue) = ???                                     // 6
+    def read(value: JsValue) = ???            // 6
   }
 }
